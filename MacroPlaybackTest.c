5bfef818385547c1a7622028aece81b1a5eda962
MarkSill <MarkSill@users.noreply.github.com>
2015-01-10 14:52:09 -0800

    Create MacroPlaybackTest.c

---
+++ MacroPlaybackTest.c
@@ -0,0 +1,18 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Motor,  mtr_S1_C1_1,     motorD,        tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C1_2,     motorE,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_1,     motorF,        tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C2_2,     motorG,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_1,     motorH,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_2,     motorI,        tmotorTetrix, openLoop, reversed)
+
+#include "macro.h/playback.h"
+#include "JoystickDriver.c"
+
+task main()
+{
+	//quite simple, if I do say so myself
+	playback("macro1.txt", 3, 0, 8, 0);
+
+}
