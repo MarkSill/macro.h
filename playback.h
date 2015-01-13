afc0caf875ea55c671c4a3d2bc68ea26a88f634c
MarkSill <MarkSill@users.noreply.github.com>
2015-01-12 16:53:33 -0800

    More work on final version

--- playback.h
+++ playback.h
@@ -11,6 +11,7 @@ This file is licensed under MIT.
 //constants
 const int MAX_SIZE = 8;
 const int MAX_STEPS = 30000;
+const int LINE_SIZE = 8;
 
 const char letters[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 const char numbers[10] = {'1','2','3','4','5','6','7','8','9','0'};
@@ -22,7 +23,8 @@ TFileHandle handle;
 TFileIOResult result;
 
 typedef struct {
-	char array[5];
+	char array[LINE_SIZE];
+	byte hello;
 } arrayChar;
 
 #endif
@@ -30,77 +32,163 @@ typedef struct {
 int step = 0;
 
 void playback(char *filename, int startMtr, int startSrv, int endMtr, int endSrv);
-int getNumber(arrayChar line);
+int getNumber(arrayChar lines);
 
 void playback(char *filename, int startMtr, int startSrv, int endMtr, int endSrv) {
 	int size;
-	int msize = 32;
-	char lines[32][5];
+	const int msize = 64;
+	char *lines[msize];
 	char incoming;
-	int line = 0, int pos = 0;
+	int line = 0;//, pos = 0;
 	OpenRead(handle, result, filename, size);
 	bool isValue = false;
+
+	string t = "";
+	sprintf(t, "File size is %d bytes", size);
+	writeDebugStreamLine(t);
 	
+	for (int i = 0; i < msize; i++) {
+		char *hi = "";
+		lines[i] = hi;
+	}
+
 	for (int i = 0; i < size; i++) {
 		ReadByte(handle, result, incoming);
 		if (incoming == LF || incoming == CR) {
 			if (incoming == LF) {
 				line++;
-				pos = 0;
+				//pos = 0;
 			}
 		} else {
-			lines[i][pos] = incoming;
-			pos++;
-			string h = "";
-			sprintf(h, "%c", incoming);
-			writeDebugStream(h);
+			char *empty = "";
+			char *in = empty + incoming;
+			strcat(lines[line], in); //I need to figure out how to add a char to a char * very badly!!
+			/*if (pos <= LINE_SIZE-1) {
+				lines[line] += incoming;
+			} else {
+				writeDebugStreamLine("pos >= LINE_SIZE!");
+			}
+			pos++;*/
 		}
 	}
 	Close(handle, result);
-	string rslt = "";
-	sprintf(rslt, "Result: %d", result);
-	writeDebugStreamLine(rslt);
-	for (int i = 0; i < msize; i++) {
-		writeDebugStreamLine(lines[i]);
+	if (result != 0) {
+		string rslt = "";
+		sprintf(rslt, "ERROR: %d", result);
+		writeDebugStreamLine(rslt);
 	}
-	
+
 	//actual processing
 	bool isLetter = false;
-	int mode = 0;
+	int mde = 0;
 	int waiting = 0;
 	for (int lne = 0; lne < msize; lne++) {
-		char line[5] = lines[lne];
+		mde = 0;
+		char *line;
+		line = lines[lne];
+		bool con = false;
+		writeDebugStreamLine(line);
+		/*
+		for (int n = 0; n < LINE_SIZE; n++) {
+			bool found = false;
+			for (int i = 0; i < 26; i++) {
+				if (lines[lne][n] == letters[i]) {
+					string hi = "";
+					sprintf(hi, "%c, %c", line[n], letters[i]);
+					//writeDebugStreamLine(hi);
+					found = true;
+				}
+			}
+			for (int i = 0; i < 10; i++) {
+				if (lines[lne][n] == numbers[i]) {
+					found = true;
+					string hi = "";
+					sprintf(hi, "%c, %c", line[n], numbers[i]);
+					writeDebugStreamLine(hi);
+				}
+			}
+			if (lines[lne][n] == 'W') {
+				//writeDebugStreamLine("W");
+				found = true;
+			}
+			if (found) {
+				//writeDebugStreamLine("Found!");
+				line[n] = lines[lne][n];
+			} else {
+				line[n] = ' ';
+			}
+			con = !found;
+		}
+		if (con) {
+			//writeDebugStreamLine("Continuing!");
+			continue;
+		}
+		writeDebugStreamLine(line);
 		for (int ltr = 0; ltr < 26; ltr++) { //check to see if it's a letter
 			if (letters[ltr] == line[0]) {
 				isLetter = true;
-				mode = ltr;
+				mde = ltr;
 				continue;
 			}
 		}
-		//check first to see if we should wait
-		if (line[0] == 'W') {
-			arrayChar arry;
-			arry.array = line;
-			wait1Msec(getNumber(arry));
-			continue;
-		}
-		//if not a letter, we arrive here at last!
 		arrayChar arry;
 		arry.array = line;
-		
+		int n = getNumber(arry);
+		//check first to see if we should wait
+		bool hasW = false;
+		for (int q = 0; q < LINE_SIZE; q++) {
+			if (line[q] == 'W') {
+				hasW = true;
+				string h = "";
+				sprintf(h, "%d", q);
+				//writeDebugStreamLine(h);
+			}
+		}
+		/*if (hasW) {
+			writeDebugStream("Waiting for ");
+			string tst = "";
+			sprintf(tst, "%d", n);
+			writeDebugStream(tst);
+			writeDebugStreamLine(" milliseconds...");
+			wait1Msec(n);
+			continue;
+		}*/
+		if (mde > 0 && mde < 9) {
+			//if not a letter, we arrive here at last!
+			string tst = "";
+			sprintf(tst, "Motor: %c", line[0]);
+			//writeDebugStreamLine(tst);
+			tst = "";
+			sprintf(tst, "Value: %d", atoi(line));
+			//writeDebugStreamLine(tst);
+			//writeDebugStreamLine(line);
+			//motor[mde] = n;
+			wait1Msec(1);
+		}
 	}
+	writeDebugStreamLine("Finished!");
 }
 
-int getNumber(arrayChar line) {
-	//needs to use atoi
-	char *lne = "";
-	for (int i = 0; i < 5; i++) {
+//this should return a number from a character array (passed through the function as a struct since you can't use arrays in functions for whatever reason)
+int getNumber(arrayChar lines) {
+	char *llne = "";
+	for (int i = 0; i < LINE_SIZE; i++) {
+		char line[LINE_SIZE] = {'1', '2', '3', '4', '5', '6', '7', '8'};
 		for (int n = 0; n < 10; n++) {
 			if (line[i] == numbers[n]) {
-				lne += line[i];
+				llne += line[i];
+				string hi = "";
+				sprintf(hi, "%c", line[i]);
+				//writeDebugStream(hi);
 				break;
 			}
 		}
 	}
-	return atoi(lne);
+	//writeDebugStreamLine("");
+	//writeDebugStreamLine(llne);
+	int n = atoi(llne);
+	string hi = "";
+	sprintf(hi, "%d", n);
+	//writeDebugStreamLine(hi);
+	return n;
 }
